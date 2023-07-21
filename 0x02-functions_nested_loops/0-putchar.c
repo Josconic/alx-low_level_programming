a17f0c3965062ef5ddca7834ba3b9263eda71cf5
Josconic <projosconic99@gmail.com>
2023-07-21 09:00:40 -0700

    learning

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char test[] = "_putchar\n";
+int i;
+for (i = 0; test[i] != '\0'; i++)
+_putchar(test[i]);
+return (0);
+}
