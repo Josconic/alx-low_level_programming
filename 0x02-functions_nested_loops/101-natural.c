be27051f555570ae91fa03b805056ea396421c3b
Josconic <projosconic99@gmail.com>
2023-07-21 14:31:03 -0700

    learning

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main : Entry point
+* print_mult_3_or_5 - Print multiples of 3 or 5
+* Return 0
+*/
+void print_mult_3_or_5(void)
+{
+int i;
+int sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+sum += i;
+}
+}
