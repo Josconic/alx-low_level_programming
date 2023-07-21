4051af50b9be8378d0a212dcab1006d41a105104
Josconic <projosconic99@gmail.com>
2023-07-21 14:56:36 -0700

    learning

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - Print numbers
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+if ((i == 50) || (i == 52))
+{
+continue;
+}
+else
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+}
