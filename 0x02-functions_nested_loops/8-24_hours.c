2934586dd84ccd18e931d99ff08d17692c756762
Josconic <projosconic99@gmail.com>
2023-07-21 11:22:22 -0700

    learning

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - Print every min of Jack Bauer day
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+int hr = 24;
+int minute = 60;
+int i = 0;
+while (i < hr)
+{
+int j = 0;
+while (j < minute)
+{
+if ((i < 10) & (j < 10))
+printf("0%d:0%d\n", i, j);
+else if ((i < 10) & (j >= 10))
+printf("0%d:%d\n", i, j);
+else if ((i >= 10) & (j < 10))
+printf("%d:0%d\n", i, j);
+else
+printf("%d:%d\n", i, j);
+j++;
+}
+i++;
+}
+}
