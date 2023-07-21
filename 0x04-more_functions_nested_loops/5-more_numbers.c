2b376b18f7ce153b95997bcc01e5f38de4c6a532
Josconic <projosconic99@gmail.com>
2023-07-21 14:58:49 -0700

    learning

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *more_numbers : print numbers 0 - 14, 10 times
+*/
+void more_numbers(void)
+{
+  int i;
+  i = 0;
+  while (i < 10)
+  {
+    for (i = 0; i <= 14; i++) {
+        _putchar('0' + i);
+        if (i >= 10)
+        {
+          _putchar('0' + 1);
+        }
+        
+    }
+    putchar('\n'); 
+    i++;
+  }
+}
