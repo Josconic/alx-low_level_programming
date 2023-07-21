44f35573a43ca18e823623b469ebea74684a12ab
Josconic <projosconic99@gmail.com>
2023-07-21 11:24:08 -0700

    learning

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - Print 9-times table
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+int i = 0;
+while (i <= 9)
+{
+int j = 0;
+int val;
+while (j <= 9)
+{
+val = i * j;
+if (j == 9)
+printf("%2d\n", val);
+else if (j == 0)
+printf("%d, ", val);
+else
+printf("%2d, ", val);
+j++;
+}
+i++;
+}
+}
