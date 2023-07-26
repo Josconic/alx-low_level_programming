c1a87a3fbb11638d360a83117f6e7c418623092d
Josconic <projosconic99@gmail.com>
2023-07-26 05:24:11 -0700

    learning

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Updates the value it points to to 98
+ * @n: Integer value to be updated.
+ * Return: Returns nothing.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
